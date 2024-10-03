2995f3848cc2eb9e67e4d780d3e152d1bf15dee9
NguyenAnhPhuc1 <zeoomega1234@gmail.com>
2024-10-03 16:26:53 +0700

    createAPIV3
    Xong appointmentAPI

---
+++ hairsalon/src/main/java/com/hairsalonbookingapp/hairsalon/api/AppointmentAPI.java
@@ -0,0 +1,70 @@
+package com.hairsalonbookingapp.hairsalon.api;
+
+import com.hairsalonbookingapp.hairsalon.entity.Appointment;
+import com.hairsalonbookingapp.hairsalon.entity.HairSalonService;
+import com.hairsalonbookingapp.hairsalon.entity.ShiftEmployee;
+import com.hairsalonbookingapp.hairsalon.entity.Slot;
+import com.hairsalonbookingapp.hairsalon.model.AppointmentRequest;
+import com.hairsalonbookingapp.hairsalon.model.AppointmentUpdate;
+import com.hairsalonbookingapp.hairsalon.model.StylistInfo;
+import com.hairsalonbookingapp.hairsalon.service.AppointmentService;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+@SecurityRequirement(name = "api")
+public class AppointmentAPI {
+
+    @Autowired
+    AppointmentService appointmentService;
+
+    @GetMapping("/service")
+    public ResponseEntity getAllAvailableServices(){
+        List<HairSalonService> list = appointmentService.getServiceList();
+        return ResponseEntity.ok(list);
+    }
+
+    @GetMapping("/stylist")
+    public ResponseEntity getAllAvailableStylist(){
+        List<StylistInfo> list = appointmentService.getAllStylistInFo();
+        return ResponseEntity.ok(list);
+    }
+
+    @GetMapping("/shift")
+    public ResponseEntity getAllAvailableStylistShift(@PathVariable String stylistId){
+        List<ShiftEmployee> list = appointmentService.getShiftEmployees(stylistId);
+        return ResponseEntity.ok(list);
+    }
+
+    @GetMapping("/slot")
+    public ResponseEntity getAllAvailableStylistSlot(@PathVariable long shiftEmployeeId){
+        List<Slot> list = appointmentService.viewAvailableSlots(shiftEmployeeId);
+        return ResponseEntity.ok(list);
+    }
+
+    @PostMapping("/appointment")
+    public ResponseEntity createNewAppointment(@Valid @RequestBody AppointmentRequest appointmentRequest){
+        Appointment appointment = appointmentService.createNewAppointment(appointmentRequest);
+        return ResponseEntity.ok(appointment);
+    }
+
+    @PutMapping("/appointment/id")
+    public ResponseEntity createNewAppointment(@Valid @RequestBody AppointmentUpdate appointmentUpdate, @PathVariable long id){
+        Appointment appointment = appointmentService.updateAppointment(appointmentUpdate, id);
+        return ResponseEntity.ok(appointment);
+    }
+
+    @DeleteMapping("/appointment/id")
+    public ResponseEntity deleteAppointment(@PathVariable long id){
+        Appointment appointment = appointmentService.deleteAppointment(id);
+        return ResponseEntity.ok(appointment);
+    }
+
+
+}
